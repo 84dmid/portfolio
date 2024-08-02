d09d1fb041f8ffdcfee671791353477a3ac0bd88
Dmitry <84dmid@gmail.com>
2024-08-02 10:23:22 +0500

    changed showing modal window

--- src/components/games/GameCard.js
+++ src/components/games/GameCard.js
@@ -7,32 +7,29 @@ const GameCard = ({ title, gameScreen, initGame }) => {
     const [isShowingGame, setShowingGame] = useState(false);
 
     return (
-        <>
-            {isShowingGame ? (
+        <Card bg="light" text="dark">
+            {isShowingGame && (
                 <GameModalWindow
                     initGame={initGame}
                     show={isShowingGame}
                     setShow={setShowingGame}
                     title={title}
                 />
-            ) : (
-                <Card bg="light" text="dark">
-                    <Card.Header>
-                        <Card.Title>{title}</Card.Title>
-                    </Card.Header>
-                    <Card.Body className="p-0">
-                        <Card.Img
-                            src={gameScreen}
-                            alt="Game Screen"
-                            style={{ height: '420px', objectFit: 'cover' }}
-                        />
-                    </Card.Body>
-                    <Card.Footer className="text-center">
-                        <Button onClick={() => setShowingGame(true)}>Play</Button>
-                    </Card.Footer>
-                </Card>
             )}
-        </>
+            <Card.Header>
+                <Card.Title>{title}</Card.Title>
+            </Card.Header>
+            <Card.Body className="p-0">
+                <Card.Img
+                    src={gameScreen}
+                    alt="Game Screen"
+                    style={{ height: '420px', objectFit: 'cover' }}
+                />
+            </Card.Body>
+            <Card.Footer className="text-center">
+                <Button onClick={() => setShowingGame(true)}>Play</Button>
+            </Card.Footer>
+        </Card>
     );
 };
 
